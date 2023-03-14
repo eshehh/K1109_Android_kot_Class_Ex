892b63ff28d93c5849a4fddc5c641e9ae056c49a
eshehh <qotjsgks1092@naver.com>
2023-03-14 16:43:33 +0900

    저장 테스트 230314 7교시

---
+++ test10/src/main/java/com/example/test10/MainActivity279.kt
@@ -0,0 +1,40 @@
+package com.example.test10
+
+import android.app.DatePickerDialog
+import android.app.TimePickerDialog
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.widget.DatePicker
+import android.widget.TimePicker
+import android.widget.Toast
+import com.example.test10.databinding.ActivityMain279Binding
+
+class MainActivity279 : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        // 바인딩 뒤에 계속 사용함.
+        // 데이팅 앱에서 , 그냥 R.id 형식으로 되어있음. 참고.
+        val binding = ActivityMain279Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.button1.setOnClickListener {
+        DatePickerDialog(this, object: DatePickerDialog.OnDateSetListener {
+            override fun onDateSet(p0: DatePicker?, p1: Int, p2: Int, p3: Int) {
+                Log.d("kkang", "year : $p1, month : ${p2+1}, dayOfMonth : $p3")
+//                Toast.makeText(this@MainActivity279, "$p1", "${p2+1}", "$p3" Toast.LENGTH_LONG).show()
+
+            }
+
+        }, 2020, 8, 21).show()
+        }
+        binding.button2.setOnClickListener {
+        TimePickerDialog(this, object: TimePickerDialog.OnTimeSetListener {
+            override fun onTimeSet(p0: TimePicker?, p1: Int, p2: Int) {
+                Log.d("kkang", "time : $p1, minute : $p2")
+
+            }
+        }, 15, 0, true).show()
+        }
+    }
+}
---
+++ test11/src/main/java/com/example/test11/MainActivity357.kt
@@ -0,0 +1,34 @@
+package com.example.test11
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
+import com.example.test11.databinding.ActivityMain357Binding
+
+class MainActivity357 : AppCompatActivity() {
+    // 토글 액션바에 버튼을 이용해서 드로워 레이아웃을 이용하는 형식.
+    lateinit var toggle: ActionBarDrawerToggle
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        //
+        val binding = ActivityMain357Binding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        //ActionBarDrawerToggle 버튼 적용
+        toggle = ActionBarDrawerToggle(this, binding.drawer, R.string.drawer_opened, R.string.drawer_closed)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        toggle.syncState()
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        //이벤트가 toggle 버튼에서 제공된거라면..
+        if(toggle.onOptionsItemSelected(item)){
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+}
