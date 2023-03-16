d5b1c4dcf4f505393badad19d5fe8c063f2bf1f7
eshehh <qotjsgks1092@naver.com>
2023-03-16 16:39:38 +0900

    저장 테스트 230316 7교시

--- test13/src/main/java/com/example/test13/MainActivity426.kt
+++ test13/src/main/java/com/example/test13/MainActivity426.kt
@@ -1,7 +1,11 @@
 package com.example.test13
 
+import android.os.Build
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.WindowInsets
+import android.view.WindowInsetsController
+import android.view.WindowManager
 import android.view.inputmethod.InputMethodManager
 import com.example.test13.databinding.ActivityMain426Binding
 
@@ -9,8 +13,28 @@ class MainActivity426 : AppCompatActivity() {
     lateinit var binding: ActivityMain426Binding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding=ActivityMain426Binding.inflate(layoutInflater)
+        binding = ActivityMain426Binding.inflate(layoutInflater)
         setContentView(binding.root)
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.R) {
+            window.setDecorFitsSystemWindows(false)
+            // 화면의 기본 장식요소 (툴바, 액션등)이 시스템 창의 경계 안으로 들어가지 않도록 설정.
+            // window.insetsController
+            val controller = window.insetsController
+            // 현재 화면에 시스템 창 장식 정보를 가져온다.
+            if (controller != null) { //널이 아니라고 하면
+                controller.hide(
+                    //  상태바, 네비게이션바 숨기는 기능
+                    WindowInsets.Type.statusBars() or
+                            WindowInsets.Type.navigationBars())
+                // 탭바 숨겨진 후 잠시 나타나도록 하는 기능
+                controller.systemBarsBehavior =
+                    WindowInsetsController.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+            }
+        } else {
+            window.setFlags(
+                WindowManager.LayoutParams.FLAG_FULLSCREEN,
+                WindowManager.LayoutParams.FLAG_FULLSCREEN)
+        }
 
         val manager = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
         binding.showInputButton.setOnClickListener {
@@ -18,8 +42,11 @@ class MainActivity426 : AppCompatActivity() {
             manager.showSoftInput(binding.editView, InputMethodManager.SHOW_IMPLICIT)
         }
         binding.hideInputButton.setOnClickListener {
-            manager.hideSoftInputFromWindow(currentFocus?.windowToken,
-                InputMethodManager.HIDE_NOT_ALWAYS)
+            manager.hideSoftInputFromWindow(
+                currentFocus?.windowToken,
+                InputMethodManager.HIDE_NOT_ALWAYS
+            )
+
         }
     }
 }
--- test13/src/main/java/com/example/test13/MainActivity443.kt
+++ test13/src/main/java/com/example/test13/MainActivity443.kt
@@ -2,11 +2,14 @@ package com.example.test13
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.Handler
+import android.os.Message
 import android.util.Log
 import com.example.test13.databinding.ActivityMain443Binding
 import kotlinx.coroutines.*
 import kotlinx.coroutines.channels.Channel
 import kotlinx.coroutines.channels.consumeEach
+import kotlin.concurrent.thread
 import kotlin.system.measureTimeMillis
 
 class MainActivity443 : AppCompatActivity() {
@@ -44,13 +47,17 @@ class MainActivity443 : AppCompatActivity() {
 //                }
 //                Log.d("kkang", "time : $time")
 //            }
+
+            // 큐 알고리즘과 비슷
             val channel = Channel<Int>()
 
+            // 스코프는 작업 2 방향 나눠서 작업.
+            // 오래 걸리는 작업.
             val backgroundScope = CoroutineScope(Dispatchers.Default + Job())
             backgroundScope.launch {
                 var sum = 0L
                 var time = measureTimeMillis {
-                    for (i in 1..2_000_000_000) {
+                    for (i in 1..2_000_000_000000) {
                         sum += i
                     }
                 }
@@ -58,6 +65,8 @@ class MainActivity443 : AppCompatActivity() {
                 channel.send(sum.toInt())
             }
 
+            // 사용자 이벤트를 처리하는 스코프, 작업의 속도가 빠르게 처리 하는 수행을 시키는게 좋습니다.
+            // 메인 스레드, UI 스레드, 화면을 구현하는 부분.
             val mainScope= GlobalScope.launch(Dispatchers.Main) {
                 channel.consumeEach {
                     binding.resultView.text = "sum : $it"
